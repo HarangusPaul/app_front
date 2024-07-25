5f11297b383def89c496f22620e35c3e4ab32d0c
Paul Harangus <p.harangus@tecro.com>
2024-07-25 15:19:50 +0300

    a

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -13,25 +13,41 @@ export const HomeComponent = () => {
     const [chat, setChat] = useState("Server is running!")
     const [input, setInput] = useState("")
     const [load, setLoad] = useState(0)
-    const [messageList, setMessageList] = useState<string[]>([])
+    const [messageList, setMessageList] = useState<string[]>(["Input"])
+
+    let list:string[] = []
 
     useEffect(() => {
         setLoad(load + 1)
     }, []);
+
+    useEffect(() => {
+        if (messageList.length === 0) {
+            console.log(messageList)
+            send()
+        }
+    }, [messageList]);
 
     const concatenateStrings = (strings: string[]): string => {
         return strings.join(' ');
     };
 
     useEffect(() => {
-        if(load === 1){
+        if (load === 1) {
             messageSubject.subscribe({
-                next:(value)=>{
-                    const messageListCopy = messageList
-                    messageListCopy.push(value)
-                    console.log(concatenateStrings(messageListCopy))
-                    setChat(concatenateStrings(messageListCopy))
-                    setMessageList(messageListCopy)
+                next: (value) => {
+
+                    let messageListCopy = messageList
+
+                    if (value === "first_new_message_to_render") {
+                        list = []
+                        return
+                    }
+                    list.push(value)
+                    setMessageList(list)
+                    console.log(concatenateStrings(list))
+                    setChat(concatenateStrings(list))
+
                 }
             })
         }
@@ -40,7 +56,7 @@ export const HomeComponent = () => {
 
 
     const send = () => {
-        return axios.post(Api+"/v1/msg",{command:input}).then((res) => {
+        return axios.post(Api + "v1/msg", {command: input}).then((res) => {
             // setChat(res.data);  // Update chat with the response
         }).catch((error) => {
             console.error("There was an error!", error);
@@ -52,16 +68,16 @@ export const HomeComponent = () => {
         <div className={"mainApp"}>
             <div className={"containerApp"}>
                 <div className={"chatApp"}>
-                    <MessageBubble message={chat} sender={"bot"} />
+                    <MessageBubble message={chat} sender={"bot"}/>
                 </div>
                 <div className={"inputApp"}>
-                    <Input size={"huge"} value={input} onChange={(value)=>{
+                    <Input size={"huge"} value={input} onChange={(value) => {
                         setInput(value.target.value)
                     }} placeholder={"Add here the text!"}/>
 
-                    <Icon name={"send"} className={'arrow'} size={"big"} onClick={()=>{
+                    <Icon name={"send"} className={'arrow'} size={"big"} onClick={() => {
                         setChat("")
-                        send()
+                        setMessageList([])
                         setInput("")
                     }}></Icon>
                 </div>
